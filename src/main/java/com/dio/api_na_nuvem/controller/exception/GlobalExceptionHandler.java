c5592351ae2b3b80b253da1d9c95967e0173d2ba
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-09-23 15:18:20 -0300

    criada exceção personalizada

---
+++ src/main/java/com/dio/api_na_nuvem/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.dio.api_na_nuvem.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>("Resource ID not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
+        return new ResponseEntity<>("Unexpected server error", HttpStatus.NOT_FOUND);
+    }
+
+}
