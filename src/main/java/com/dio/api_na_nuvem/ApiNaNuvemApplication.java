7f3235886865bfbfad0dbebf26b6b89857130908
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-09-23 13:29:13 -0300

    primeiro commit

---
+++ src/main/java/com/dio/api_na_nuvem/ApiNaNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.dio.api_na_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNaNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNaNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/api_na_nuvem/ApiNaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.api_na_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNaNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
